4f55d3ff8a2e2d166668dd92109929a9bc44b7e0
Felipe Felizardo Gonçalves <felipe.goncalves@sci10.com.br>
2020-10-14 14:59:04 +0000

    Clona automaticamente os novos projetos do projeto padrão

--- app/Template/project_creation/create.php
+++ app/Template/project_creation/create.php
@@ -15,33 +15,7 @@
         <p class="form-help"><?= t('The project identifier is optional and must be alphanumeric, example: MYPROJECT.') ?></p>
 
         <?= $this->form->label(t('Programming hours budget'), 'hour_budget') ?>
-        <?= $this->form->number('hour_budget', $values, $errors, array('autofocus')) ?>        
-
-        <?= $this->form->checkbox('per_swimlane_task_limits', t('Column task limits apply to each swimlane individually'), 1, false) ?>
-
-        <?= $this->form->label(t('Task limit'), 'task_limit') ?>
-        <?= $this->form->number('task_limit', $values, $errors) ?>
-
-        <?php if (count($projects_list) > 1): ?>
-            <?= $this->form->label(t('Create from another project'), 'src_project_id') ?>
-            <?= $this->form->select('src_project_id', $projects_list, $values, array(), array(), 'js-project-creation-select-options') ?>
-        <?php endif ?>
-
-        <div class="js-project-creation-options" <?= isset($values['src_project_id']) && $values['src_project_id'] > 0 ? '' : 'style="display: none"' ?>>
-            <p class="alert"><?= t('Which parts of the project do you want to duplicate?') ?></p>
-
-            <?php if (! $is_private): ?>
-                <?= $this->form->checkbox('projectPermissionModel', t('Permissions'), 1, true) ?>
-                <?= $this->form->checkbox('projectRoleModel', t('Custom roles'), 1, true) ?>
-            <?php endif ?>
-
-            <?= $this->form->checkbox('categoryModel', t('Categories'), 1, true) ?>
-            <?= $this->form->checkbox('tagDuplicationModel', t('Tags'), 1, true) ?>
-            <?= $this->form->checkbox('actionModel', t('Actions'), 1, true) ?>
-            <?= $this->form->checkbox('customFilterModel', t('Custom filters'), 1, true) ?>
-            <?= $this->form->checkbox('projectMetadataModel', t('Metadata'), 1, false) ?>
-            <?= $this->form->checkbox('projectTaskDuplicationModel', t('Tasks'), 1, false) ?>
-        </div>
+        <?= $this->form->number('hour_budget', $values, $errors, array('autofocus')) ?>
 
         <?= $this->modal->submitButtons() ?>
     </form>
