13cedc4788419c4b0553de406464ee0519f6ed4f
Felipe <felipe.goncalves@sci10.com.br>
2020-11-05 18:31:37 +0000

    Dá acesso ao usuário aos detalhes de um requisito

--- app/ServiceProvider/AuthenticationProvider.php
+++ app/ServiceProvider/AuthenticationProvider.php
@@ -100,6 +100,7 @@ class AuthenticationProvider implements ServiceProviderInterface
         $acl->add('ProjectStatusController', '*', Role::PROJECT_MANAGER);
         $acl->add('ProjectScopeController', '*', Role::PROJECT_MANAGER);
         $acl->add('ProjectTagController', '*', Role::PROJECT_MANAGER);
+        $acl->add('SubtaskController', 'show', Role::PROJECT_MEMBER);
         $acl->add('SubtaskController', '*', Role::PROJECT_MANAGER);
         $acl->add('SubtaskConverterController', '*', Role::APP_ADMIN);
         $acl->add('SubtaskRestrictionController', '*', Role::PROJECT_MEMBER);
