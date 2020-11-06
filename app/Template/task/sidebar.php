4d742b113e170365a509c67714d6fa9e71103459
Felipe <felipe.goncalves@sci10.com.br>
2020-11-06 19:38:38 +0000

    Limpa no menu lateral da solicitação

--- app/Template/task/sidebar.php
+++ app/Template/task/sidebar.php
@@ -24,34 +24,9 @@
         <?= $this->hook->render('template:task:sidebar:information', array('task' => $task)) ?>
     </ul>
 
-    <div class="sidebar-title">
-        <h2><?= t('Actions') ?></h2>
-    </div>
-    <ul>
-        <li>
-            <?= $this->modal->medium('code-fork', t('Add internal link'), 'TaskInternalLinkController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-        </li>
-        <li>
-            <?= $this->modal->medium('external-link', t('Add external link'), 'TaskExternalLinkController', 'find', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-        </li>
-        <li>
-            <?= $this->modal->small('comment-o', t('Add a comment'), 'CommentController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-        </li>
-        <li>
-            <?= $this->modal->medium('file', t('Attach a document'), 'TaskFileController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-        </li>
-        <li>
-            <?= $this->modal->medium('camera', t('Add a screenshot'), 'TaskFileController', 'screenshot', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-        </li>
-        <li>
-            <?= $this->modal->small('paper-plane', t('Send by email'), 'TaskMailController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-        </li>
-        <?= $this->hook->render('template:task:sidebar:actions', array('task' => $task)) ?>
-    </ul>
-
     <?php if ($this->user->hasProjectAccess('TaskModificationController', 'edit', $task['project_id'])): ?>
     <div class="sidebar-title">
-        <h2><?= t('Edit') ?></h2>
+        <h2><?= t('Manage') ?></h2>
     </div>
     <ul>
         <?php if ($this->projectRole->canUpdateTask($task)): ?>
@@ -67,22 +42,6 @@
         <li>
             <?= $this->modal->small('clone', t('Move to another project'), 'TaskDuplicationController', 'move', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
         </li>
-        <?php if ($task['is_active'] == 1 && $this->projectRole->isSortableColumn($task['project_id'], $task['column_id'])): ?>
-            <li>
-                <?= $this->modal->small('arrows', t('Move position'), 'TaskMovePositionController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-            </li>
-        <?php endif ?>
-        <?php if ($this->projectRole->canChangeTaskStatusInColumn($task['project_id'], $task['column_id'])): ?>
-            <?php if ($task['is_active'] == 1): ?>
-                <li>
-                    <?= $this->modal->confirm('times', t('Close this task'), 'TaskStatusController', 'close', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-                </li>
-            <?php else: ?>
-                <li>
-                    <?= $this->modal->confirm('check-square-o', t('Open this task'), 'TaskStatusController', 'open', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
-                </li>
-            <?php endif ?>
-        <?php endif ?>
         <?php if ($this->projectRole->canRemoveTask($task)): ?>
             <li>
                 <?= $this->modal->confirm('trash-o', t('Remove'), 'TaskSuppressionController', 'confirm', array('task_id' => $task['id'], 'project_id' => $task['project_id'], 'redirect' => 'board')) ?>
--- app/Template/task_external_link/show.php
+++ app/Template/task_external_link/show.php
@@ -7,4 +7,6 @@
             'project' => $project,
         )) ?>
     </div>
+    <?= $this->modal->mediumButton('external-link', t('Add external link'), 'TaskExternalLinkController', 'find', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+    
 </details>
